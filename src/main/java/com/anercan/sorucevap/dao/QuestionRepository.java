9c3df1b97fd88d504777e98ffd80bbf104ea0591
anercan <anercan@gmail.com>
2019-11-28 23:55:42 +0300

    PostgreSQL Connection

--- src/main/java/com/anercan/sorucevap/dao/QuestionRepository.java
+++ src/main/java/com/anercan/sorucevap/dao/QuestionRepository.java
@@ -1,16 +1,13 @@
 package com.anercan.sorucevap.dao;
 
-import com.anercan.sorucevap.entity.Category;
 import com.anercan.sorucevap.entity.Question;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.Collection;
 import java.util.List;
 
 public interface QuestionRepository extends BaseRepository<Question, Long> {
 
-    @Query(value = "select * from questions where id in (SELECT question_id FROM category_relations WHERE category_id=:id)",nativeQuery = true)
+    @Query(value = "select * from questions where id in (SELECT question_id FROM category_relations WHERE category_id=:id)", nativeQuery = true)
     List<Question> getListByCategory(Long id);
 
 
