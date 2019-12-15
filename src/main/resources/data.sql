b1d45b8e7e92a35f911d7d26a83734ba71d392b1
anercan <anercan@gmail.com>
2019-12-15 22:37:04 +0300

    PostgreSQL Configuration

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into users values (1,current_date ,true ,'anercan@mail.com','123456',5,'anercan');
+insert into users values (2,current_date ,true ,'test1@mail.com','123456',5,'test1');
+insert into users values (3,current_date ,true ,'test2@mail.com','123456',5,'test2');
